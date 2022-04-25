2324aa1393a3c34ef5b9cdfd987c36cd31973d76
Hermilo Ortega <hermilo.jh@gmail.com>
2022-04-25 01:08:49 -0700

    Add Initial Source Code with .NET Core 3.1 Web App MVC and Identity

---
+++ Source Code/PetAdopt/PetAdopt/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PetAdopt.Data;
+
+[assembly: HostingStartup(typeof(PetAdopt.Areas.Identity.IdentityHostingStartup))]
+namespace PetAdopt.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
